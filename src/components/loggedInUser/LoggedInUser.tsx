9de0bd55d16c6bb7e9345f5d56d27b86357e28aa
smullen <113264284+smullen2022@users.noreply.github.com>
2022-10-23 18:14:51 -0400

    have user name and logout display working

--- src/components/loggedInUser/LoggedInUser.tsx
+++ src/components/loggedInUser/LoggedInUser.tsx
@@ -1,14 +1,23 @@
 import React, { useContext } from 'react';
+import Button from '@mui/material/Button';
 import { UserContext } from '../userContext/UserContext';
+import styles from './LoggedInUser.module.css';
 
 interface ILoggedInUserProps {}
 
 const LoggedInUser: React.FunctionComponent<ILoggedInUserProps> = () => {
   const { user, logOut } = useContext(UserContext);
+  const buttonStyle = {
+    lineHeight: '1rem',
+    fontSize: '.75rem',
+    marginLeft: '.5rem'
+  };
   return user ? (
-    <div>
-      <span>{`${user.firstName} ${user.lastName}`}</span>
-      <button onClick={() => logOut && logOut()}>log out</button>
+    <div className={styles.loggedInContainer}>
+      <span>{`Welcome, ${user.firstName} ${user.lastName}`}</span>
+      <Button sx={buttonStyle} onClick={() => logOut && logOut()}>
+        Log out
+      </Button>
     </div>
   ) : null;
 };
